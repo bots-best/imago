fce59577e716304f7540840717f5d76dde99a7b6
Justus K <justus.k@protonmail.com>
2020-08-19 00:13:19 +0200

    feat(models): add first basic models

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#![deny(rust_2018_idioms)]
+#![warn(clippy::pedantic)]
+
+pub mod models;
---
+++ src/models.rs
@@ -0,0 +1,42 @@
+//! All models of the imago web server.
+
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Hash, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Id {
+    id: Uuid,
+}
+
+impl Into<Uuid> for Id {
+    fn into(self) -> Uuid {
+        self.id
+    }
+}
+
+impl From<Uuid> for Id {
+    fn from(id: Uuid) -> Self {
+        Self { id }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ImageUploadRequest {
+    pub url: String,
+    pub formats: Vec<ImageFormat>,
+    pub sizes: Vec<ImageSize>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ImageFormat {
+    Png,
+    Jpeg,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ImageSize {
+    pub height: u32,
+    pub width: u32,
+}
